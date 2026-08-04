e92287117046ce4371d3ff034e2f2c34198cddc2
Elena Pavlova <lenlho22@gmail.com>
2026-08-04 15:15:02 +0300

    feat(api): register GetUserProfileHandler in UserModule providers
    Wires the query handler into UserModule's providers array so
    @nestjs/cqrs's QueryBus can dispatch GetUserProfileQuery from
    UserController.

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -3,11 +3,12 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { AuthModule } from '../auth/auth.module';
 import { CreateUserHandler } from './commands/handlers/create-user.handler';
 import { FindUserByEmailHandler } from './queries/handlers/find-user-by-email.handler';
+import { GetUserProfileHandler } from './queries/handlers/get-user-profile.handler';
 import { UserCreatedHandler } from './events/handlers/user-created.handler';
 import { UserController } from './user.controller';
 
 const CommandHandlers = [CreateUserHandler];
-const QueryHandlers = [FindUserByEmailHandler];
+const QueryHandlers = [FindUserByEmailHandler, GetUserProfileHandler];
 const EventHandlers = [UserCreatedHandler];
 
 @Module({
